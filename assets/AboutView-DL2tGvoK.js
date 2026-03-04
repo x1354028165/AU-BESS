5c552d7873bff85a25ca39160bbd1901a3cda8e1
moss-xxh <moss-xxh@users.noreply.github.com>
2026-03-04 14:33:30 +0800

    Phase5C-final + perf

---
+++ assets/AboutView-DL2tGvoK.js
@@ -0,0 +1,1 @@
+import{_ as s,b as t,c as o,e as a}from"./index-COPCsoTg.js";const n={},c={class:"about"};function r(_,e){return t(),o("div",c,[...e[0]||(e[0]=[a("h1",null,"This is an about page",-1)])])}const l=s(n,[["render",r]]);export{l as default};
