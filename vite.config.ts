6697964871684db5d9fd8ddf00a428dcfec4ae15
moss-xxh <moss-xxh@users.noreply.github.com>
2026-03-04 09:18:12 +0800

    ci: 配置GitHub Actions部署到moss-xxh/AU-01

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AU-BESS/',
+  base: '/AU-01/',
   plugins: [
     vue(),
     vueDevTools(),
